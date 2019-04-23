1291610faf85b0f16915611f84c986bc6f710530
Softwarica College Lab <softwarica@softwarica.com>
2019-04-23 11:06:18 +0545

    Initial commit

---
+++ app/src/main/java/com/e/fragment/MainActivity.java
@@ -0,0 +1,38 @@
+package com.e.fragment;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import Fragment.FirstFragment;
+
+public abstract class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnFragment;
+    private Boolean status = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnFragment = findViewById(R.id.btnFragment1);
+        btnFragment.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (status){
+            FirstFragment firstFragment = new FirstFragment();
+            fragmentTransaction.add(R.id.fragmentContainer,firstFragment);
+            fragmentTransaction.commit();
+            btnFragment.setText("Load Second Fragment");
+
+        }
+
+
+    }
+}
